6501cf1d2d81874ed88fea81de633ec3be4b1fde
Arthur Varlet <arthur.varlet@insa-strasbourg.fr>
2024-10-26 10:38:34 +0200

    modif fonction extraire_donnees_fichier() pour remplacer None par la dernière position connue

--- simpleParser.py
+++ simpleParser.py
@@ -17,6 +17,7 @@ def extraire_donnees_fichier(fichier):
     return : liste des positions successives [X,Y,Z], elements = None si pas de deplacement dans une direction'''
 
     donnees = []
+    lastPos = [0,0,0,0] #liste pour sauvegarder les dernières positions sur chaque axe
     # Ouvrir le fichier pour la lecture
     with open(fichier, 'r') as f:
         lignes = f.readlines()
@@ -36,13 +37,13 @@ def extraire_donnees_fichier(fichier):
 
                 # Extraire les valeurs et les mettre dans une liste, mettre None si une valeur est manquante
                 valeurs = [
-                    float(x.group(1)) if x else None,
-                    float(y.group(1)) if y else None,
-                    float(z.group(1)) if z else None,
-                    float(e.group(1)) if e else None
+                    float(x.group(1)) if x else lastPos[0],
+                    float(y.group(1)) if y else lastPos[1],
+                    float(z.group(1)) if z else lastPos[2],
+                    float(e.group(1)) if e else lastPos[3]
                 ]
                 # Vérifie que l'on a au moins une instruction de déplacement en X, Y, Z ou E (exclue les commandes Feedrate)
-                if any (val is not None for val in valeurs):
+                if any (val is not 0 for val in valeurs):
                     # Ajouter cette ligne de valeurs à la liste de données
                     donnees.append(valeurs)
 
@@ -56,25 +57,26 @@ def calculDirectionDepl(donnees):
 
     directions = []
     for i in range (1,len(donnees)):
-            # Calculer les différences si la ligne contient X ou Y
-            if donnees[i][0] is not None and donnees[i][1] is not None:
-                # Si pas de position précédente avec X et Y, on remonte en arrière jusqu'à la dernière position connue
-                goback=1
-                #Tant que l'on a pas trouvé la position précédente
-                while donnees[i-goback][0] is None or donnees[i-goback][1] is None: 
-                    goback+=1
-                # Si on a une position précédente avec X et Y, on calcule la différence (arrondie à 5 décimales)
-                diffX = round(donnees[i][0] - donnees[i-goback][0],5)
-                diffY = round(donnees[i][1] - donnees[i-goback][1],5)
+            # Calculer les différences si la ligne contient X ou Y ou Z
+            if donnees[i][0] is not None or donnees[i][1] is not None or donnees[i][2] is not None:
+                # Si pas de position précédente, on remonte en arrière jusqu'à la dernière position connue
+                gobackX, gobackY, gobackZ = 0
+                # Parcourir la liste à l'envers
+                for i in range(len(donnees) - 1, -1, -1):
+                    if donnees[i][0] is not None:
+                        posLastX = i #indice de la dernière position non vide en X
+                    if donnees[i][1] is not None:
+                        posLastY = i #indice de la dernière position non vide en Y
+                    if donnees[i][2] is not None:
+                        posLastZ = i #indice de la dernière position non vide en Z
+                    if (posLastX!=0 and posLastY!=0 and posLastZ!=0):
+                        break  # Arrêter la boucle dès que la dernière valeur non vide est trouvée pour chaque coordonnées
+                
+                # Si on a une position précédente X,Y,Z, on calcule la différence (arrondie à 5 décimales)
+                diffX = round(donnees[i][0] - donnees[posLastX][0],5)
+                diffY = round(donnees[i][1] - donnees[posLastY][1],5)
+                diffZ = round(donnees[i][1] - donnees[posLastZ][1],5)
                 directions.append([diffX,diffY,0])
-            # Calculer la différence si la ligne contient Z
-            if donnees[i][2] is not None:
-                goback=1
-                while donnees[i-goback][2] is None:
-                    goback+=1
-                # Si on a une position précédente en Z, on calcule la différence (arrondie à 5 décimales)
-                diffZ = round(donnees[i][2] - donnees[i-goback][2],5)
-                directions.append([0,0,diffZ])
     return directions
 
 #-----------------------BOUCLE PRINCIPALE-------------------------#
--- tests/tests.py
+++ tests/tests.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from simpleParser import extraire_donnees_fichier
 
 
-#Test 1 : l'extraction fonctionne bien comme demandé
+#Test 1 : l'extraction fonctionne bien comme demandé avec le gcode généré par Cura et Slic3r
 file = "inputs\\pyramide1_Cura_FlavorRepetier_RamsaiParameters.gcode"
 output = extraire_donnees_fichier(file)
 for ligne in output:
