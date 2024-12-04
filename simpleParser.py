447ee31249b52462c46de07eeeb6375fdda81d9d
Thomas SIMONCELLI <tsimoncelli@unistra.fr>
2024-12-04 17:49:06 +0100

    Ajout interpretation E et mise en forme
    Le E du gcode est maintenant utilisé pour mettre à l'état 1 ou 0 une sortie numérique du flange KUKA. Cette sortie numérique permet de contrôler la viprohead en tout ou rien avec la carte de commande BIFASI-POC2.
    Les données exportées sont mise en forme de tel manière que le copier/coller dans sunrise est simplifier (motion batch inclus dans des méthodes java).

--- simpleParser.py
+++ simpleParser.py
@@ -21,7 +21,7 @@ def extraire_donnees_fichier(fichier):
     return : liste des positions successives [X,Y,Z], elements = None si pas de deplacement dans une direction'''
 
     donnees = []
-    lastPos = [0,0,0,0] #liste pour sauvegarder les dernières positions sur chaque axe
+    lastPos = [0,0,0,0] #liste pour sauvegarder les dernieres positions sur chaque axe
     # Ouvrir le fichier pour la lecture
     with open(fichier, 'r') as f:
         lignes = f.readlines()
@@ -31,7 +31,7 @@ def extraire_donnees_fichier(fichier):
             # Traduit la commande G28 = "home all axis" en une position [0,0,0]
             if ligne.startswith('G28'):
                 donnees.append([0.0,0.0,0.0,lastPos[3]])
-            # Vérifier si la ligne commence par G1 ou G0 = "linear move"
+            # Verifier si la ligne commence par G1 ou G0 = "linear move"
             elif ligne.startswith(('G1','G0')):
                 # Utiliser une regex pour trouver les valeurs X,Y,Z,E
                 x = re.search(r'X([-\d.]+)', ligne)
@@ -46,9 +46,9 @@ def extraire_donnees_fichier(fichier):
                     float(z.group(1)) if z else lastPos[2],
                     float(e.group(1)) if e else lastPos[3]
                 ]
-                # Vérifie que l'on a au moins une instruction de déplacement différente des précédentes (exclue les commandes Feedrate)
+                # Verifie que l'on a au moins une instruction de deplacement differente des precedentes (exclue les commandes Feedrate)
                 if not utils.utils.listesIdentiques(valeurs, lastPos):
-                    # Ajouter cette ligne de valeurs à la liste de données
+                    # Ajouter cette ligne de valeurs e la liste de donnees
                     donnees.append(valeurs)
                     lastPos = valeurs
 
@@ -62,11 +62,11 @@ def calculDirectionDepl(donnees):
 
     directions = []
     for i in range (1,len(donnees)):
-        # Calcule les déplacements relatifs dans chaque direction (arrondis à 5 digits)
+        # Calcule les deplacements relatifs dans chaque direction (arrondis e 5 digits)
         diffX = round(donnees[i][0] - donnees[i-1][0],5)
         diffY = round(donnees[i][1] - donnees[i-1][1],5)
         diffZ = round(donnees[i][2] - donnees[i-1][2],5)
-        diffE = round(donnees[i][3] - donnees[i-1][3],5)
+        diffE = round(donnees[i][3],5)
         directions.append([diffX,diffY,diffZ,diffE])
     return directions
 
@@ -78,16 +78,60 @@ def export_commandes_robot(fichier,vecteurs,repere,vit_lin):
     vit_lin : vitesse lineaire lors des translations [mm/s]
     return : None'''
 
-    # Créé une sous liste sans les infos en E (dernière colonne)
-    xyzPos = [vect[:3] for vect in vecteurs]
-    # Ouvrir un fichier en mode écriture
+    # Cree une sous liste sans les infos en E (derniere colonne)
+    xyzPos = [vect[:4] for vect in vecteurs]
+    oldE = 0
+    i = 1 #numÃ©ro du motionbatch
+    j = 1
+    resetLigne = 0
+    # Ouvrir un fichier en mode ecriture
     with open(fichier, "w") as f:
+        #Ajout d'une ligne initiale pour la creation de methode JAVA motionbatch
+        f.write("public void mb1(){" + "\n")
+        f.write("double blend = 4;" + "\n" )
+        f.write("MotionBatch mb191 = new MotionBatch(" + "\n")
         # Parcourir chaque ligne du tableau
         for pos in xyzPos:
-            # Si c'est un déplacement linéaire en X, Y ou Z
-            if (pos[0]!=0 or pos[1]!=0 or pos[2]!=0):
-                # Ecrit la commande robot pour les déplacements donnés dans le fichier 
-                f.write("linRel(Transformation.ofDeg(" + ",".join(map(str, pos)) + ",0.0,0.0,0.0),getApplicationData().getFrame(" + "\"" + repere + "\")).setCartVelocity(" + str(vit_lin) + ")," + "\n") 
+            if (pos[3]>0 and oldE==0): #Si E positif, on clos le motionbatch et on active l'extrusion puis on ouvre le motionbatch_suivant
+                f.write("\n" + ").setBlendingCart(blend);" + "\n")
+                f.write("_lbr.move(mb191);" + "\n")
+                f.write("_medflange.setOutputX3Pin2(true);" + "\n")
+                f.write("}" + "\n" + "\n")
+                i = i + 1
+                resetLigne = 0
+                f.write("public void mb"+ str(i) + "(){" + "\n")
+                f.write("double blend = 4;" + "\n" )
+                f.write("MotionBatch mb191 = new MotionBatch(" + "\n")
+                oldE = 1
+            if (pos[3]<0 and oldE==1): #Si E negatif, on dÃ©sactive l'extrusion
+                f.write("\n" + ").setBlendingCart(blend);" + "\n")
+                f.write("_lbr.move(mb191);" + "\n")
+                f.write("_medflange.setOutputX3Pin2(false);"+ "\n")
+                f.write("}" + "\n" + "\n")
+                i = i + 1
+                resetLigne = 0
+                f.write("public void mb"+ str(i) + "(){" + "\n")
+                f.write("double blend = 4;" + "\n" )
+                f.write("MotionBatch mb191 = new MotionBatch(" + "\n")
+                oldE = 0    # Si c'est un deplacement lineaire en X, Y ou Z
+            if ((pos[0]!=0 or pos[1]!=0 or pos[2]!=0) and resetLigne==1):
+                # Ecrit la commande robot pour les deplacements donnes dans le fichier 
+                f.write("," + "\n" + "linRel(Transformation.ofDeg(" + ",".join(map(str, pos[:3])) + ",0.0,0.0,0.0),getApplicationData().getFrame(" + "\"" + repere + "\")).setCartVelocity(" + str(vit_lin) + ")") 
+            if ((pos[0]!=0 or pos[1]!=0 or pos[2]!=0) and resetLigne==0):
+                # Ecrit la commande robot pour les deplacements donnes dans le fichier 
+                f.write("linRel(Transformation.ofDeg(" + ",".join(map(str, pos[:3])) + ",0.0,0.0,0.0),getApplicationData().getFrame(" + "\"" + repere + "\")).setCartVelocity(" + str(vit_lin) + ")") 
+                resetLigne = 1
+            
+
+        f.write(").setBlendingCart(blend);" + "\n")
+        f.write("_lbr.move(mb191);" + "\n")
+        f.write("_medflange.setOutputX3Pin2(false);"+ "\n")
+        f.write("}" + "\n" + "\n")
+        f.write("Appel de methode a mettre dans le run " + "\n")
+        while i > 0 :
+            f.write("mb"+str(j)+"();"+"\n")
+            i = i - 1
+            j = j + 1
 
 def choixFichierEntree(liste):
     ''' Fonction qui permet la selection d'un fichier parmi une liste de noms de fichiers dans le terminal
@@ -106,11 +150,11 @@ def choixFichierEntree(liste):
 def runConsole(inFolder,outFile,repere):
     ''' Fonction prenant en charge la configuration du parser de g-code via un terminal'''
     print("Convertisseur de G-code en code robot KUKA")
-    # Récupère et laisse l'utilisateur sélectionner le fichier d'entrée de son choix
+    # Recupere et laisse l'utilisateur selectionner le fichier d'entree de son choix
     listeFichier = utils.utils.getFilesIn(inFolder)
     inFile = inFolder + "\\" + choixFichierEntree(listeFichier)
     print("fichier gcode entree = " + inFile)
-    # Affiche les différents paramètres du parser de G-Code
+    # Affiche les differents parametres du parser de G-Code
     print("fichier commandes robot KUKA sortie = " + outFile)
     print("\ndeplacements robot dans repere = " + repere)
     vitesse = input("Entrez vitesse max. de deplacement [mm/s] :")
